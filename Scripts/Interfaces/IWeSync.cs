0dcb0ff5c105c3737857dd161763904203176871
Nakata Nobuyuki <nobu.nakata@gmail.com>
2022-06-23 14:27:38 +0900

    moved interfaces from example project

---
+++ Scripts/Interfaces/IWeSync.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace WeSyncSys {
+
+	public interface IWeSync {
+		WeSpace Space { get; }
+		WeTime Time { get; }
+	}
+}
